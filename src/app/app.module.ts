9bb846f41f7b9b89f4ae8bce1462906d923b359a
GojkoMaric <gojko.maric@yahoo.com>
2018-02-19 18:51:02 +0100

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MyGalleriesComponent } from './components/my-galleries/my-galleries.component';
+import { CreateGalleryComponent } from './components/create-gallery/create-gallery.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LoginComponent,
+    RegisterComponent,
+    MyGalleriesComponent,
+    CreateGalleryComponent
   ],
   imports: [
     BrowserModule
